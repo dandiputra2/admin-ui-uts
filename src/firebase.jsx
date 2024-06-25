57640738f9643e32c1761c3d521ed37cc911a4ae
unknown <111202113684@mhs.dinus.ac.id>
2024-06-25 16:34:42 +0700

    login logout

---
+++ src/firebase.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "store-tutorial-f38ac.firebaseapp.com",
+  projectId: "store-tutorial-f38ac",
+  storageBucket: "store-tutorial-f38ac.appspot.com",
+  messagingSenderId: "1029528862683",
+  appId: "1:1029528862683:web:8118b5474c29d810f471fe"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
